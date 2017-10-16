e04dcc33a9abf6659852f13271b51bb79c803e51
Brian Sheen <bsheen@gmail.com>
2017-10-15 19:09:06 -0500

    solved valid paretheses order

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,53 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String s = "){";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        StringBuilder parenthOrder = new StringBuilder();
+        if (s.length() == 0) {
+            return true;
+        }
+        if(s.length()%2==1){
+            return false;
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '{') {
+                parenthOrder.append("}");
+            }
+            if (s.charAt(i) == '[') {
+                parenthOrder.append("]");
+            }
+            if (s.charAt(i) == '(') {
+                parenthOrder.append(")");
+            }
+            if (s.charAt(i) == '}') {
+                if(parenthOrder.length()==0 || parenthOrder.charAt(parenthOrder.length()-1)!='}'){
+                    return false;
+                } else {
+                    parenthOrder.deleteCharAt(parenthOrder.length()-1);
+                }
+            }
+            if (s.charAt(i) == ']') {
+                if(parenthOrder.length()==0 || parenthOrder.charAt(parenthOrder.length()-1)!=']'){
+                    return false;
+                } else {
+                    parenthOrder.deleteCharAt(parenthOrder.length()-1);
+                }
+            }
+            if (s.charAt(i) == ')') {
+                if(parenthOrder.length()==0 || parenthOrder.charAt(parenthOrder.length()-1)!=')'){
+                    return false;
+                } else {
+                    parenthOrder.deleteCharAt(parenthOrder.length()-1);
+                }
+            }
+        }
+        if(parenthOrder.length()==0) {
+            return true;
+        }
+        return false;
     }
 }
